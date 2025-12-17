29f97038d286f2836b0cca67af6e29344dbe1e7f
Miika Alonen <amiika@gmail.com>
2025-12-18 00:45:52 +0200

    new config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
-export default {
-  // config options
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dictionaries', // Source directory
+          dest: '.'            // Destination in dist (root)
+        }
+      ]
+    })
+  ]
+});
